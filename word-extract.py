74215d2696e5f7e27cad92f22a188a150112b304
AbcEric <hc_lee@qq.com>
2018-11-06 23:57:15 +0800

    V1.1
    Add comments

--- word-extract.py
+++ word-extract.py
@@ -6,17 +6,23 @@ base_dir = "./img/"
 dst_dir = "./output/"
 #min_val = 10
 #min_range = 30
-min_val = 10
-min_range = 30
+
+# 字体颜色？
+min_val = 30000
+# 每行的最小高度
+min_range = 24
 
 count = 0
 
+# 判断有多少行：向右投影？必须水平，没有干扰！
 def extract_peek(array_vals, minimun_val, minimun_range):
     start_i = None
-    end_i = None
+    #end_i = None
     peek_ranges = []
+    print(array_vals)
 
     for i, val in enumerate(array_vals):
+        #print(i, val)
         if val > minimun_val and start_i is None:
             start_i = i
         elif val > minimun_val and start_i is not None:
@@ -29,7 +35,7 @@ def extract_peek(array_vals, minimun_val, minimun_range):
                 # 代表行数：有几行
                 peek_ranges.append((start_i, end_i))
                 start_i = None
-                end_i = None
+                #end_i = None
         elif val < minimun_val and start_i is None:
             pass
         else:
@@ -37,7 +43,7 @@ def extract_peek(array_vals, minimun_val, minimun_range):
 
     return peek_ranges
 
-# 提取每个字?
+# 提取每行的文字
 def cutImage(img, peek_ranges):
     global count
     for i, peek_range in enumerate(peek_ranges):
@@ -54,28 +60,39 @@ def cutImage(img, peek_ranges):
             new_shape = (150, 150)
             img1 = cv2.resize(img1, new_shape)
             cv2.imwrite(dst_dir + str(count) + ".png", img1)
-            # cv2.rectangle(img, pt1, pt2, color)
+            cv2.rectangle(img, pt1, pt2, (55,255,155))
 
 for fileName in os.listdir(base_dir):
     print(fileName)
+    #if fileName != "japen.png":
+    if fileName != "6408.bmp":
+        continue
+
     img = cv2.imread(base_dir + fileName)
     # 转为灰度图像：
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 二值化：二值化后方便处理图片像素灰度值
+    # 自适应阈值(cv2.adaptiveThreshold):一种局部性的阈值，通过规定一个区域大小，比较这个点与区域大小里面像素点
+    # 的平均值（或者其他特征）的大小关系确定这个像素点是属于黑或者白（如果是二值情况）
+    # 简单阈值cv2.threshold，简单阈值是一种全局性的阈值，只需要规定一个阈值值，整个图像都和这个阈值比较。
+    #
     # 第一个原始图像
     # 第二个像素值上限
     # 第三个cv2.ADAPTIVE_THRESH_GAUSSIAN_C ：领域内像素点加权和，权重为一个高斯窗口
-    # # 第四个值的赋值方法：只有cv2.THRESH_BINARY 和cv2.THRESH_BINARY_INV
-    # # 第五个Block size:规定领域大小（一个正方形的领域）
-    # # 第六个常数C，阈值等于均值或者加权值减去这个常数（为0相当于阈值 就是求得领域内均值或者加权值）
+    # 第四个值的赋值方法：只有cv2.THRESH_BINARY 和cv2.THRESH_BINARY_INV
+    # 第五个Block size:规定领域大小（一个正方形的领域）
+    # 第六个常数C，阈值等于均值或者加权值减去这个常数（为0相当于阈值，就是求得领域内均值或者加权值）
     adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
                                                cv2.THRESH_BINARY_INV, 11, 2)
     cv2.imwrite("g_" + fileName, gray)
+    print("THRESH=", adaptive_threshold[2])
+    print("THRESH=", adaptive_threshold[5])
 
+    # axis=1以后就是将一个矩阵的每一行向量相加: 相当于向右投影
     horizontal_sum = np.sum(adaptive_threshold, axis=1)
 
-    # print("H_SUM=", horizontal_sum)
+    #print("H_SUM=", horizontal_sum)
+
     peek_ranges = extract_peek(horizontal_sum, min_val, min_range)
     line_seg_adaptive_threshold = np.copy(adaptive_threshold)
 
@@ -88,6 +105,7 @@ for fileName in os.listdir(base_dir):
         pt1 = (x, y)
         pt2 = (x + w, y + h)
         cv2.rectangle(line_seg_adaptive_threshold, pt1, pt2, 255)
+
     vertical_peek_ranges2d = []
 
     # 根据垂直和水平方向像素与空间阈值确定方格?
@@ -96,10 +114,11 @@ for fileName in os.listdir(base_dir):
         end_y = peek_range[1]
         line_img = adaptive_threshold[start_y:end_y, :]
         vertical_sum = np.sum(line_img, axis=0)
-        vertical_peek_ranges = extract_peek(
-            vertical_sum, min_val, min_range)
+        vertical_peek_ranges = extract_peek(vertical_sum, min_val, min_range)
         vertical_peek_ranges2d.append(vertical_peek_ranges)
 
+    #print(vertical_peek_ranges2d)
     cutImage(img, peek_ranges)
+    cv2.imwrite("k_" + fileName, img)
 
 # https://www.jianshu.com/p/64808391285e
